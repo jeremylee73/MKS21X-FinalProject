1cc218106b11e0c8af1ed8c0113de53723e93fa0
Jeremy Lee <jlee00@stuy.edu>
2019-01-04 14:44:32 -0500

    added StoreVals and added toString

--- GraphingCalculator.java
+++ GraphingCalculator.java
@@ -1,20 +1,21 @@
 public class GraphingCalculator {
-  private int[][] graph1;
-  private int[][] graph2;
+  private boolean[][] graph1;
+  private boolean[][] graph2;
 
   public GraphingCalculator(){
-
+    graph1 = new boolean[21][21];
+    storeVals(1, 0, graph1);
   }
 
-  // public void storeVals(int x, int c, int[][] graph){
-  //   for (int i=-10; i<=10; i++){
-  //     for (int j=-10; j<=10; j++){
-  //       if (i == (x*j) + c){
-  //         graph[j] =
-  //       }
-  //     }
-  //   }
-  // }
+  public static void storeVals(int x, int c, boolean[][] graph){
+    for (int i=0; i<=20; i++){
+      for (int j=0; j<=20; j++){
+        if ((i-10) == (x*(j-10)) + c){
+          graph[i][j] = true;
+        }
+      }
+    }
+  }
 
   public static double solve(double x1, double c1, double x2, double c2){
     double newX = x2 - x1;
@@ -22,8 +23,31 @@ public class GraphingCalculator {
     return (newC * 1.0) / newX;
   }
 
+  public String toString(){
+    String ans = "";
+    for (int i=0; i<graph1.length; i++){
+      ans += "{";
+      for (int j=0; j<graph1[i].length; j++){
+        if (graph1[i][j]){
+          ans += (i-10) + "," + (j-10) + "}";
+        }
+      }
+      if (i != graph1.length - 1){
+        ans += ", ";
+      } else {
+        ans += "}";
+      }
+    }
+    return ans;
+  }
+
   public static void main(String[] args){
+    // Testing solve
     System.out.println(solve(1, 1, 2, 2));
+
+    // Testing storeVals
+    GraphingCalculator graph = new GraphingCalculator();
+    System.out.println(graph);
   }
 
 }
