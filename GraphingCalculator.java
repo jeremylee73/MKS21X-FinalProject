486938c81d04b1aac967c81a8a7b83c4b3e7723e
Jeremy Lee <jlee00@stuy.edu>
2019-01-09 19:05:56 -0500

    Added findRoots toGraphingCalculator  and getX and getC in SingleGraph

--- GraphingCalculator.java
+++ GraphingCalculator.java
@@ -4,10 +4,14 @@ import javax.imageio.ImageIO;
 
 public class GraphingCalculator {
 
-  public static double solve(double x1, double c1, double x2, double c2){
+  public static double solve(double x1, int c1, double x2, int c2){
     double newX = x2 - x1;
     double newC = c1 - c2;
     return (newC * 1.0) / newX;
+  }
+
+  public static double findRoots(double x1, int c1){
+    return solve(x1, c1, 0, 0);
   }
 
   public static void main(String[] args){
@@ -17,6 +21,8 @@ public class GraphingCalculator {
     // Testing storeVals
     SingleGraph output = new SingleGraph(2001,2001,2.0,100);
     // System.out.println(graph);
+
+    System.out.println("Root: (" + findRoots(output.getX(), output.getC()) + ",0)");
 
     // // Testing dilates
     output.dilateUpDown(5);
@@ -30,6 +36,8 @@ public class GraphingCalculator {
     output.rotate90C();
     output.rotate90C();
     output.rotate90CC();
+
+    System.out.println("Root: (" + findRoots(output.getX(), output.getC()) + ",0)");
 
     // Testing display
     output.display();
